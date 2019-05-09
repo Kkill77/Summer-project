03fd0cf1725d82741449efd991d6a447932af3dc
Kkill77 <34036324+Kkill77@users.noreply.github.com>
2019-05-09 14:45:49 -0600

    Add files via upload

---
+++ code/main.py
@@ -0,0 +1,43 @@
+import pygame
+import sys
+from menu_screen import menuScreen
+from my_beast import myBeastScreen
+from other_beast import otherBeastScreen
+# setting up pygame
+pygame.init()
+
+# initializing display and clock
+pygame.display.set_caption("Code Challenge")
+clock = pygame.time.Clock()
+
+quit = False
+
+# main game loop
+def game_loop(quit):
+    while not quit:
+        # handling quit
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                # We can add an "Are you sure you would like to quit" window later... 
+                quit = True
+        # default run state is the menu screen
+        gameState = menuScreen(None)
+
+        if gameState == "quit":
+            quit = True
+        elif gameState == "myBeast":
+            # running tutorial screen
+            quit = myBeastScreen()
+        elif gameState == "otherBeast":
+            # running play game
+            quit = otherBeastScreen()
+        # updating display and establishing FPS
+        pygame.display.update()
+        clock.tick(60)
+
+# calling game loop
+game_loop(quit)
+
+# quitting appropriately when done playing
+pygame.quit()
+sys.exit()
