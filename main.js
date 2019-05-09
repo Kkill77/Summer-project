08545fc9f2549249d39192bde20123291a93ea4b
Kkill77 <34036324+Kkill77@users.noreply.github.com>
2019-05-09 13:28:24 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+"use strict";
+console.log("Hello there");
+
+class Beast {
+	constructor(id, name, species, age, sex) {
+		this.id = id;
+		this.name = name;
+		this.species = species;
+		this.age = age;
+		this.sex = sex;
+	}
+}
+
+let allBeasts = new Set();
+let myBeasts = new Set();
+
+
+
+function main() {
+
+}
